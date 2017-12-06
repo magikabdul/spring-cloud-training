ef6905eb2c1b6777569596a286abb8e94356a4bb
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-12-06 08:37:23 +0100

    Docker

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
+cd configuration
 mvn clean
 mvn package
-cd client
+cd ../departments
+mvn clean
+mvn package
+cd ../discovery
+mvn clean
+mvn package
+cd ../gateway
+mvn clean
+mvn package
+cd ../users
+mvn clean
+mvn package
+cd ../zipkin
+mvn clean
+mvn package
+cd ../client
 rm -rf target
 ng build --prod
