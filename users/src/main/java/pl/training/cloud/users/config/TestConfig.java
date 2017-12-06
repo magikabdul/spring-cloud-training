fd2ae7771f956fcdb142541cf47deeae202bc033
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-12-06 10:12:56 +0100

    Users

---
+++ users/src/main/java/pl/training/cloud/users/config/TestConfig.java
@@ -0,0 +1,20 @@
+package pl.training.cloud.users.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RefreshScope
+public class TestConfig {
+
+    private Long defaultDepartmentId;
+
+    @Value("#{defaultDepartmentId}")
+    public void setDefaultDepartmentId(Long defaultDepartmentId) {
+        Logger.getLogger(getClass().getName()).log(Level.INFO, " ### Default department id: " + defaultDepartmentId);
+        this.defaultDepartmentId = defaultDepartmentId;
+    }
+
+}
