29aab09b2008f75abb70d38050dad4940c6c5e4b
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-12-21 14:09:57 +0100

    2

--- rebuild.sh
+++ rebuild.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
-cd configuration
 mvn clean
 mvn package
-cd ../departments
-mvn clean
-mvn package
-cd ../discovery
-mvn clean
-mvn package
-cd ../gateway
-mvn clean
-mvn package
-cd ../users
-mvn clean
-mvn package
-cd ../zipkin
-mvn clean
-mvn package
-cd ../client
+cd client
 rm -rf target
 ng build --prod
